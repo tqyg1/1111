cd02943ebb8da60346d2e4d70be50e9a4a2d9aca
tqyg1 <122511722+tqyg1@users.noreply.github.com>
2023-01-14 01:12:49 +0800

    Add files via upload

--- pages/weather/weather.js
+++ pages/weather/weather.js
@@ -1,66 +1,47 @@
-// pages/weather/weather.js
+//index.js
+//这里只获取了实况天气信息，没有用预测信息
+//获取应用实例
+const app = getApp()
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    adcode:'',
+    city:'',
+    humidity:'',
+    province:'',
+    reporttime:'',
+    temperature:'',
+    weather:'',
+    winddirection:'',
+    windpower:'',
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  onLoad:function(){
+    var self = this;
+    wx.request({
+      url: 'https://restapi.amap.com/v3/weather/weatherInfo',
+      data:{
+        'key': 'ee83d9611abbe7401caa23b55e0d3663',//改为自己申请的Key
+        'city': '120102',
+        'extensions': 'base'
+      },
+      header:{
+        'content-type': 'application/json'
+      },
+      success:function(res){
+        console.log(res.data);
+        self.setData({
+          adcode: res.data.lives[0].adcode,
+          city: res.data.lives[0].city,
+          humidity: res.data.lives[0].humidity,
+          province: res.data.lives[0].province,
+          temperature: res.data.lives[0].temperature,
+          reporttime: res.data.lives[0].reporttime,
+          weather: res.data.lives[0].weather,
+          winddirection: res.data.lives[0].winddirection,
+          windpower: res.data.lives[0].windpower
+        })
+      }
+    })
   }
 })
