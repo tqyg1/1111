7bff73119f0d29bd1327e01e4b761517c72007af
8000800 <105056174+8000800@users.noreply.github.com>
2023-01-15 04:57:55 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// app.js
+///app.js
 App({
-  onLaunch() {
+  onLaunch: function () {
     // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
+    var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
@@ -12,8 +12,30 @@ App({
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
   },
   globalData: {
-    userInfo: null
+    key: '256e042a4d4d480091c4435087fe0473',//和风天气的key,自行去申请，谢谢
+    cityId:'101010100',//城市id
+    cityName:'北京市',//城市名
   }
 })
